1ef70e2ef07b05b17cc7bfd026221e4cf1874594
Fabien Chbd <fabien.chabaud1@gmail.com>
2023-09-07 11:13:33 +0200

    Test components

---
+++ src/components/Calcul.cy.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Calcul from "./Calcul";
+
+describe("<Calcul />", () => {
+  it("renders", () => {
+    cy.mount(<Calcul />);
+    cy.get("h1").should("have.text", "Générateur de Calcul Mental");
+    cy.get(".numbers > p").should("contain.text", "+");
+    cy.get(".numbers > p").should("contain.text", "=");
+    cy.get("input").should("be.visible").and("have.text", "");
+    cy.get("h2").should("have.text", "T'es sur de toi ???");
+    cy.get("button").should("have.text", "Valider...");
+    cy.get(".score").should("have.text", "Score : 0");
+    cy.get(".good-reponse").should("not.exist");
+    cy.get(".wrong-reponse").should("not.exist");
+    cy.get("goodReponse").should("not.exist");
+    cy.get("badReponse").should("not.exist");
+  });
+});
